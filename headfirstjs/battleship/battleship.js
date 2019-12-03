4016bf9005fd0c39111b93258a36d43b016e33e8
Jeffrey Elkner <jeff@elkner.net>
2019-12-03 14:04:23 -0500

    Update battleship homepage

--- headfirstjs/battleship/battleship.js
+++ headfirstjs/battleship/battleship.js
@@ -32,5 +32,3 @@ while (isSunk == false) {
 
 var stats = "You took " + guesses + " guesses to sink the battleship, " +
             "which means your shooting accuracy was " + (3/guesses);
-
-alert(stats);
