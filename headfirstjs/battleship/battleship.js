5baaf72135e57c7aec3860f645fdaf2b4aa2aefb
Jeffrey Elkner <jeff@elkner.net>
2019-12-03 15:20:01 -0500

    Add background image to battleship

--- headfirstjs/battleship/battleship.js
+++ headfirstjs/battleship/battleship.js
@@ -6,26 +6,26 @@ var guess;
 var hits = 0;
 var guesses = 0;
 
-var isSunk = false;
+var isSunk = true;
 
 while (isSunk == false) {
-  guess = prompt("Ready, aim, fire! (enter a number from 0-6): ");
+  // guess = prompt("Ready, aim, fire! (enter a number from 0-6): ");
 
   if (guess < 0 || guess > 6) {
-    alert("Please enter a valid cell number!");
+    // alert("Please enter a valid cell number!");
   } else {
     guesses++;
 
     if (guess == location1 || guess == location2 || guess == location3) {
       hits++;
-      alert("HIT!");
+      // alert("HIT!");
     } else {
-      alert("MISS");
+      // alert("MISS");
     }
 
     if (hits == 3) {
         isSunk = true;
-        alert("You sank my battleship!");
+        // alert("You sank my battleship!");
     }
   }
 }
